5ae9575c670961879d2550cf6f401282e3e43ee9
Daniel <vansdan@web.de>
2022-05-07 13:30:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='cbpi4-ilc-sensor',
+      version='0.0.1',
+      description='Generic CraftBeerPi HTTP Sensor Plugin',
+      author='Daniel Lauterbach',
+      author_email='vansdan@web.de',
+      url='https://github.com/Vansdan/cbpi4-ilc-sensor',
+      license='GPLv3',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4-ilc-sensor': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-ilc-sensor'],
+      install_requires=[
+            'cbpi>=4.0.0.34',
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+     )
