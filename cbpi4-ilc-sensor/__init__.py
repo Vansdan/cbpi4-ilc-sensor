4aebac33d5a9fbe407e2b9ba045791c85d9a0166
Daniel <vansdan@web.de>
2022-05-09 10:29:55 +0200

    Update __init__.py

--- cbpi4-ilc-sensor/__init__.py
+++ cbpi4-ilc-sensor/__init__.py
@@ -14,36 +14,24 @@ import time
 logger = logging.getLogger(__name__)
 
 
-@parameters([Property.Text(label="IP_ILC", configurable=True, default_value="192.168.1.152"), 
-             Property.Text(label="Variable_ILC", configurable=True, default_value="ANALOG_KG.WP_DS")
-            ])
-
-
-   class CustomSensor(CBPiSensor):
+@parameters([])
+class CustomSensor(CBPiSensor):
     
     def __init__(self, cbpi, id, props):
-    
         super(CustomSensor, self).__init__(cbpi, id, props)
         self.value = 0
 
-    
     @action(key="Test", parameters=[])
     async def action1(self, **kwargs):
-     
-    self.ip_ilc = self.props.get("IP_ILC")
-    self.variable_ilc = self.props.get("Variable_ILC") 
-      
-      print("ACTION!", kwargs)
+        print("ACTION!", kwargs)
 
     async def run(self):
-
         while self.running is True:
             self.value = random.randint(0,50)
             self.push_update(self.value)
             await asyncio.sleep(1)
     
     def get_state(self):
-        # return the current state of the sensor
         return dict(value=self.value)
 
 
