498300a402a5524155e78364b6d5bf8b746c040a
Daniel <vansdan@web.de>
2022-05-09 12:55:08 +0200

    Update __init__.py

--- cbpi4-ilc-sensor/__init__.py
+++ cbpi4-ilc-sensor/__init__.py
@@ -16,8 +16,8 @@ logger = logging.getLogger(__name__)
 
 @parameters([
 
-    Property.Text(label="IP ILC", configurable=True, description="IP Adress of ILC SPS (example: 192.168.1.150)"),
-    Property.Text(label="Sensor Variable", configurable=True, description="Sensor Variable in SPS (example: SENSORS.SENSOR1"),
+    Property.Text(label="IP ILC", configurable=True, description="IP Adress of ILC SPS (example: 192.168.1.150)", default_value="192.168.1.152"),
+    Property.Text(label="Sensor Variable", configurable=True, description="Sensor Variable in SPS (example: SENSORS.SENSOR1", default_value="ANALOG_KG.WP_DS"),
     Property.Number(label="Continuous Interval", configurable=True, description="Refresh interval in seconds used in continuous mode")
 
 
@@ -34,9 +34,16 @@ class CustomSensor(CBPiSensor):
 
     async def run(self):
         while self.running is True:
-            self.value = random.randint(0,50)
+            
+            ip_ilc = self.props.get("IP ILC")
+            variable_ilc = self.props.get("Sensor Variable")            
+            url = "http://" + ip_ilc + "/cgi-bin/readVal.exe?" + variable_ilc
+            r = requests.get(url)
+            
+            #self.value = random.randint(0,50)
+            self.value = r.text
             self.push_update(self.value)
-            await asyncio.sleep(1)
+            await asyncio.sleep(5)
     
     def get_state(self):
         return dict(value=self.value)
